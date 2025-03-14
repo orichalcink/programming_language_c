5b87ef9393221d825c761964579a72f1d0c69130
orichalcink <orichalcink@gmail.com>
2025-03-14 08:56:21 +0200

    Added more token support to lexer.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # programming_language_c
-Programming language made in C.
+Programming language made in C by chalcinxx.
+
+## Features
+### Comments:
+```c
+// This is a single line comment.
+/*
+ * This is a
+ * multi line
+ * comment.
+*/
+string name; // Your name
+```
+This is valid by choice:
+```c
+/* This is an
+ * unfinished
+ * comment
+
+string name; // Nothing after the comment gets executed
+```
--- include/errors/errors.h
+++ include/errors/errors.h
@@ -5,5 +5,11 @@ static char *err_invalid_input = "Invalid input.";
 
 static char *err_unexpected_char = "Unexpected character while lexing.";
 static char *err_cannot_resize_ident = "Could not resize identifier while lexing.";
+static char *err_cannot_resize_string = "Could not resize string while lexing.";
+static char *err_cannot_resize_number = "Could not resize number while lexing.";
+static char *err_invalid_character = "Invalid character while lexing, single quotes \"'\" can only contain one character (escape characters are an exception, as they require 2 characters instead).";
+static char *err_invalid_escape_code = "Invalid escape code while lexing.";
+static char *err_invalid_quotes = "Invalid quote placement in a number while lexing.";
+static char *err_invalid_real = "Invalid real number with multiple commas while lexing.";
 
 #endif // ERRORS_H
